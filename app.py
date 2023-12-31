37cd95668d3e0a9eadf58d3024a170d9826eb873
Dashiell <dashiellrich@gmail.com>
2023-12-31 14:44:58 -0500

    converted from bad practices, to slightly better ones

--- app.py
+++ app.py
@@ -13,71 +13,29 @@ load_dotenv()
 RIOT_KEY = os.getenv('RIOT_KEY')
 DISCORD_KEY = os.getenv('DISCORD_KEY')
 
+client = RiotAPIClient(default_headers={"X-Riot-Token": f"{RIOT_KEY}"})
 intents = discord.Intents.default()
 intents.messages = True
 intents.guild_messages = True
 intents.message_content = True
 
-discord_client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents)
 
+#TODO: implement specific recent check // IN-PROGRESS
+#      implement removal from json file // DONE
+#      make the recent matches prettier // IN-PROGRESS
+#      add average placement to recent games // NOT-STARTED
+#      add current rank to recent games // NOT-STARTED
+#      implement discord.ext bot commands // DONE
+#      finish implementing stats menu // IN-PROGRESS
 
-#TODO: implement specific recent  check
-#      implement removal from json file
-#      make the recent matches prettier
-#      add average placement to recent games
-#      add current rank to recent games
-#      
-
-
-@discord_client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {discord_client.user}')
-    loop.start()
+    print(f'We have logged in as {bot.user}')
 
-@tasks.loop(seconds=5)
-async def loop():
-    try:
-        cur_channel = discord_client.get_channel(1190789769865723954)  # Use an integer for the channel ID
-        if cur_channel:
-            print(f"Channel Acquired: {cur_channel}")
-            with open("users.json") as f:
-                data = json.load(f)
-            for item in data:
-                print(item["username"], item["puuid"])
-        else:
-            print("Channel not found.")
-    except Exception as e:
-        print(f"Error: {e}")
-
-@discord_client.event
-async def on_message(message):
-    if message.author == discord_client.user:
-        return
-    if message.content.startswith('$add'):
-            try:
-                print("Attempt Received")
-                summoner = await add_user(message.content, message.author.name)
-                await message.channel.send(f'Successfully added {summoner["name"]} to database')
-            except Exception as e:
-                print(f"Error: {e}")
-                await message.channel.send("Error Occurred: Check Logs")
-    if message.content.startswith('$commands'):
-        await message.channel.send('Commands:\n Add User: Format <$add username#tag>')
-    if message.content.startswith('$recent'):
-        try:
-            print("Attempting to grab recent games") 
-            output = await recent_games(message.content, message.author.name) 
-            await message.channel.send(output)
-        except Exception as e:
-           print(f"Error: {e}")
-
-async def add_user(message, author):
-    components = message.split()
-    if len(components) == 2 and '#' in components[1]:
-        username, tag = components[1].split('#', 1)
-    else: 
-        raise ValueError("Invalid username and tag format")
-
+@bot.command(name='add')
+async def add(ctx, args):
+    username, tag = args.split('#', 1)
     async with RiotAPIClient(default_headers={"X-Riot-Token": f"{RIOT_KEY}"}) as client:
         account = await client.get_account_v1_by_riot_id(region="americas", game_name=username, tag_line=tag)
         if account is None:
@@ -97,7 +55,7 @@ async def add_user(message, author):
             raise ValueError("User already exists")
         # Append new data
         user_data = {
-            "discord_account": author,
+            "discord_account": ctx.author.name,
             "username": username,
             "tag": tag,
             "puuid": account["puuid"]
@@ -106,31 +64,61 @@ async def add_user(message, author):
         # Write updated data
         with open('users.json', 'w') as json_file:
             json.dump(data, json_file, indent=2)
+    await ctx.send(f"Successfully added user: {username}#{tag}")
     return summoner
-async def recent_games(message, author):
-    with open("users.json") as f:
+
+@bot.command(name='remove')
+async def remove(ctx, args):
+    username, tag = args.split('#', 1)
+    with open("users.json", 'r') as f:
         data = json.load(f)
-        cur_puuid = next((item["puuid"] for item in data if item["discord_account"] == author), None)
 
-    if cur_puuid is None:
-        return "User not found."
+    removed = False
+    for item in data:
+        if item["discord_account"] == ctx.author.name and item["username"]==username and item["tag"]==tag:
+            data.remove(item)
+            removed = True
+            break
+    if removed:
+        with open("users.json", 'w') as f:
+            json.dump(data, f)
+        await ctx.send(f"Succesfully removed {username}#{tag} from the database.")
+    else:
+        await ctx.send("You do not have permission to delete that user or the user does not exist.")
 
-    async with RiotAPIClient(default_headers={"X-Riot-Token": f"{RIOT_KEY}"}) as client:
-        if message == "$recent":
-            match_ids = await client.get_tft_match_v1_match_ids_by_puuid(region="americas", puuid=cur_puuid)
+@bot.command(name='recent')
+async def recent(ctx):
+        found = False
+        with open("users.json", 'r') as f: 
+            data = json.load(f)
+        for item in data:
+            if item["discord_account"]==ctx.author.name:
+                puuid = item["puuid"]
+                found = True
+                break
+        if found:
             placements = []
-            async with TaskGroup(asyncio.Semaphore(100)) as tg: 
-                for match_id in match_ids[:20]:
-                    await tg.create_task(client.get_tft_match_v1_match(region="americas", id=match_id)) 
-            matches = tg.results() 
-
+            async with RiotAPIClient(default_headers={"X-Riot-Token": f"{RIOT_KEY}"}) as client:
+                match_ids = await client.get_tft_match_v1_match_ids_by_puuid(region="americas", puuid=puuid)
+                async with TaskGroup(asyncio.Semaphore(100)) as tg: 
+                    for match_id in match_ids[:20]:
+                        await tg.create_task(client.get_tft_match_v1_match(region="americas", id=match_id)) 
+                matches = tg.results() 
             for match in matches:
                 # Find the participant with the current puuid
                 for participant in match['info']['participants']:
-                    if participant['puuid'] == cur_puuid:
+                    if participant['puuid'] == puuid:
                         placements.append(participant['placement'])
                         break  # Break the loop once the participant is found
+            await ctx.send(f"Here are your 20 most recent games: {placements}")
+@bot.command(name='stats')
+async def stats(ctx, args=None):
+    if (args != None):
+        args = ctx.author.name
 
-        return placements
 
-discord_client.run(f"{DISCORD_KEY}")
+@bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send("pong")
+
+bot.run(f"{DISCORD_KEY}")
